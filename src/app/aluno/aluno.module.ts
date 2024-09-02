011912b3c696d44bf33de3ba5fc48bc5b2e15139
Sergio Felix <sergiofelix9@gmail.com>
2024-09-02 15:58:52 -0300

    feat: Add AlunoService to AlunoModule providers

--- src/app/aluno/aluno.module.ts
+++ src/app/aluno/aluno.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { AlunoService } from './services/aluno.service';
 
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule
+  ],
+  providers: [
+    AlunoService
   ]
 })
 export class AlunoModule { }
