2fb82b17df0b21fe8cc730f866e71212fa03e7d8
Sergio Felix <sergiofelix9@gmail.com>
2024-09-02 16:52:56 -0300

    feat: Add RouterModule and FormsModule to AlunoModule imports

--- src/app/aluno/aluno.module.ts
+++ src/app/aluno/aluno.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 import { AlunoService } from './services/aluno.service';
 import { ListarAlunoComponent } from './listar-aluno/listar-aluno.component';
-
 
 @NgModule({
   declarations: [
     ListarAlunoComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule,
+    FormsModule
   ],
   providers: [
     AlunoService
