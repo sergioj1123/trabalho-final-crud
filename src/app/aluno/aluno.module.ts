53af827edba00075cfa15b2c5ae64d99ad07e310
Sergio Felix <sergiofelix9@gmail.com>
2024-09-02 16:10:47 -0300

    feat: Add ListarAlunoComponent to AlunoModule declarations

--- src/app/aluno/aluno.module.ts
+++ src/app/aluno/aluno.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AlunoService } from './services/aluno.service';
+import { ListarAlunoComponent } from './listar-aluno/listar-aluno.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ListarAlunoComponent
+  ],
   imports: [
     CommonModule
   ],
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListarAlunoComponent } from './aluno/listar-aluno/listar-aluno.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'alunos/listar', pathMatch: 'full'},
+  {path: "alunos", redirectTo: "alunos/listar"},
+  {path: "alunos/listar", component: ListarAlunoComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
